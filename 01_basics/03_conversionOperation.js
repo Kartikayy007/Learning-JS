713945cc7b64b8faa8c0a24d24465f33528a396d
kartikay6969 <130781246+kartikay6969@users.noreply.github.com>
2024-03-27 00:53:23 +0000

    learnt about conversion operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,23 @@
-let score = "22abc"
+let score = null
 
 console. log (typeof score);
  console. log (typeof(score));
 
-let valueinNumber = Number(score)
+let valueinNumber = Number(score) //function to convert  any to number
 console.log(typeof valueinNumber);
 console.log(valueinNumber)
+
+// "33" = 33
+// "33abc" = NaN
+// undefined = NaN
+// true = 1 && false = 0
+// null = 0
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// when "" is empty then false
+// else true 
+// 1 = true
+// 0 = false
