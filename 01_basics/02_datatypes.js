b4f07887e1f27e582ec269341ecb539e5d964073
kartikay6969 <130781246+kartikay6969@users.noreply.github.com>
2024-03-27 00:30:17 +0000

    learnt about data types and conversion operations

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+let name = "Kartikay" // string (words)
+let age = 21 // integer type (any number)
+let isLoggedIn = true // boolean (true/false)
+let gender
+// undefined = when the value is not assigned (here gender is undefined)
+let dateOfBirth = null
+// null = the variable is empty 
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof isLoggedIn);
+console.log(typeof age)
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,8 @@
+let score = "22abc"
+
+console. log (typeof score);
+ console. log (typeof(score));
+
+let valueinNumber = Number(score)
+console.log(typeof valueinNumber);
+console.log(valueinNumber)
