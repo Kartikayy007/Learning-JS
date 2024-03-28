c8d5f87277af258e2e4a661699a02f279b8fd126
kartikay6969 <130781246+kartikay6969@users.noreply.github.com>
2024-03-28 12:33:28 +0000

    learnt about heaps and stacks

---
+++ 01_basics/stacksAndHeap.js
@@ -0,0 +1,9 @@
+//stack memory and heap memory
+//stack is used in all primtive data types
+//heap in all the non-primitive data types
+
+let Name = "kartikay"
+let sirname = Name
+sirname = "singh"
+console.log(Name)
+console.log(sirname)
