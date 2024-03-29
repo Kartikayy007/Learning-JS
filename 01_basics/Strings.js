d40d6b2ca49719a6943a124e4ca523f5665835bb
kartikay6969 <130781246+kartikay6969@users.noreply.github.com>
2024-03-29 16:05:43 +0000

    learnt about strings and staring numbers and maths

--- 01_basics/Strings.js
+++ 01_basics/Strings.js
@@ -8,4 +8,9 @@ console.log(`hello my name is ${Name} and I have ${amount} rs`) //mordern syntax
 const gameName = new String('kartikay')
 console.log(gameName[0])
 
-console.log(Name.length) 
+console.log(Name.length) //for checking length
+console.log(Name.toUpperCase(Name)) //for upr case
+console.log(Name.charAt(0)) // for checking charater at
+console.log(Name.indexOf("y")) //to check index
+console.log(Name.trim()) // to remove extra spaces
+console.log(Name.substring(1,6)) // from index 1 to 6
