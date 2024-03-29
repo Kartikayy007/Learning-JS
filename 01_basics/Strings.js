88bd6501cb42073ed9bfa83f46d3f650511751c9
kartikay6969 <130781246+kartikay6969@users.noreply.github.com>
2024-03-29 15:25:58 +0000

    learnt about strings

---
+++ 01_basics/Strings.js
@@ -0,0 +1,11 @@
+let Name = "kartikay"
+let amount = 50
+
+// console.log(Name + amount) (outdated syntax)
+
+console.log(`hello my name is ${Name} and I have ${amount} rs`) //mordern syntax
+
+const gameName = new String('kartikay')
+console.log(gameName[0])
+
+console.log(Name.length) 
