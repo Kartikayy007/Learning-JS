bc9d309ff3f1dc8649c22e4302fab8f9b83aa0db
kartikay6969 <130781246+kartikay6969@users.noreply.github.com>
2024-03-29 16:37:18 +0000

    learnt about num and maths working in js

---
+++ 01_basics/numberAndMaths.js
@@ -0,0 +1,14 @@
+const score = 400
+console.log(score);
+
+
+const balance = new Number(100) 
+//specifically defining number
+console.log(balance);
+
+console.log(score.toString())
+console.log(typeof score)
+
+const Newnum = 20.99
+console.log(Newnum.toPrecision(2));
+// to be precise
