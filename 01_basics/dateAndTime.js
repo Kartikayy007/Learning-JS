762e1c39b9b8cc253b00b09f3a9f97b2fb75fdc8
Kartikay Singh <130781246+kartikay6969@users.noreply.github.com>
2024-03-30 10:10:35 +0000

    learnt about dates in javascript

---
+++ 01_basics/dateAndTime.js
@@ -0,0 +1,11 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+//different ways to represent, represent date and time
+
+console.log(typeof myDate) //object
+
+let myCreatedDate = new Date(2024,3,4)
+console.log(myCreatedDate.toDateString())
