75cff138d7eab8d8b9aa576b71409d3acd382e25
Kartikay Singh <130781246+kartikay6969@users.noreply.github.com>
2024-04-01 05:18:50 +0000

    learnt array

---
+++ 01_basics/Arrays.js
@@ -0,0 +1,16 @@
+// Arrays
+
+const arr = [1,2,3,"kartikay"]
+
+console.log(arr[1])
+console.log(arr[3])
+
+arr.push("singh")
+arr.push(6)
+arr.push(9)
+
+arr.unshift(0)
+arr.shift()
+arr.pop()
+console.log(arr.includes(3))
+console.log(arr)
