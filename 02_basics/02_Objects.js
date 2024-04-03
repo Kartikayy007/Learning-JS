b981e34670714bd09f6e186b90d5b67cd492cf25
Kartikay Singh <130781246+kartikay6969@users.noreply.github.com>
2024-04-03 14:49:15 +0000

    learning apis

--- 02_basics/02_Objects.js
+++ 02_basics/02_Objects.js
@@ -29,3 +29,17 @@ const obj2 = {
     4: "d",
 } // second obj
 
+// const obj3 = Object.assign({}, obj1, obj2)
+// Adding two objects
+// here {} is target not necessary to put but if not i.e. Object.assign(obj1, obj2) then all the values will be assigned to obj1
+
+// console.log(obj3);
+
+const obj3 = {...obj1, ...obj2}
+// other way to add object
+
+console.log(obj3) 
+console.log(Object.values(student)) // shows values in array
+console.log(Object.entries(student)) // shows values in array with entries too
+let arr = Object.entries(student)
+console.log(arr[1][1])
---
+++ 02_basics/03_Objects.js
@@ -0,0 +1,10 @@
+const course = {
+    Name: "js",
+    price: 999,
+    Teacher: "kartikay"
+}
+
+const {Teacher} = course // way to extract from object
+
+console.log(Teacher)
+
