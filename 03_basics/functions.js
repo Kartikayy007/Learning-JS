ec8a60a8f475a27c6a7cd1ebb76bdcf5a6be29c9
Kartikay Singh <130781246+kartikay6969@users.noreply.github.com>
2024-04-07 12:11:03 +0000

    learned

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -7,24 +7,24 @@ sayMyName // Refrence of the function
 sayMyName() // Execution of function
 
 // function addingTwoNumbers(num1, num2){
-//     console.log(num1 + num2)
+//  console.log(num1 + num2)
 // }
 
 function addingTwoNumbers(num1, num2){ // it is parameter (num1 and num2)
     let result = num1 + num2 //store them and resturn then print the sum is the correct way of doing things
     return result
     //or we can directly type 
-    //return num1 + num2 both are same
+    //Return num1 + num2 both are same
 }
 //After return nothing prints
 // return is the end
-const result = addingTwoNumbers(1, 2) // 1 and 2 are arguments
+const result = addingTwoNumbers(1, 2) 
+// 1 and 2 are arguments
 console.log(result)
 
 function loginUserMessage(username) {
-    return `${username} just logged in`
+    return console.log(`${username} just logged in`);
 }
-console.log('heloo')
-console.log(loginUserMessage("kartikay"))
 
-console.log("hello world")
+console.log(loginUserMessage("kartikay"))
+loginUserMessage('karti')
