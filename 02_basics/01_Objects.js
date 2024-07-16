fbd1829d3623c544021f85034c01a75f6bf93326
Kartikay Singh pundir <130781246+kartikay6969@users.noreply.github.com>
2024-07-16 04:30:28 +0000

    retracking

--- 02_basics/01_Objects.js
+++ 02_basics/01_Objects.js
@@ -2,20 +2,20 @@
 
 const jsuser = {
     Name: "kartikay",
-    age: 18,
+    age: 28,
     location: "delhi",
-    email: "karitkay@google.com",
+    email: "kartik@google.com",
     isLoggedIn: true,
     lastLoginDays: ["monday","saturday"]
 }
 
 console.log(jsuser.age)
-console.log(jsuser["age"]) 
-//can also be accsessed using sqaure notations
+console.log(jsuser["age"])  //can also be accsessed using sqaure notations
 
 jsuser.age = 19
 // to update values
 
-Object.freeze(jsuser)
+Object.freeze(jsuser) 
 // freezes object now no values can be updated
-
+jsuser.age = 12;
+console.log(jsuser.age)
