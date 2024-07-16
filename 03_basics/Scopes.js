bbadeaf3d9a8707f32030da3a9670f50be2bb07d
Kartikay Singh pundir <130781246+kartikay6969@users.noreply.github.com>
2024-07-16 07:41:57 +0000

    revised

--- 03_basics/Scopes.js
+++ 03_basics/Scopes.js
@@ -23,3 +23,11 @@ function one() {
     two() // execution of two() function
 }
 one() // execution of one() function
+
+function addThree(num1, num2, num3) {
+    console.log(num1 + num2 + num3)
+}
+let num1 = 2;
+let num2 = 3;   
+let num3 = 4;
+addThree(num1, num2, num3) // 2,3,4 are arguments
---
+++ 03_basics/functionWIthObject.js
@@ -0,0 +1,4 @@
+function cart(num1) {
+    return num1
+}
+console.log(cart(1)) 
